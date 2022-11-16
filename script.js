2460d94850c6f2faccbebda0bbeb13ce5480be56
Gabriel Freitas <gabrielkelvinfreitas@gmail.com>
2022-11-16 19:08:17 -0300

    REQ 5 - Finalizado, função para verificar input criado

--- script.js
+++ script.js
@@ -5,10 +5,13 @@ const buttonCreate = document.getElementById('criar-carta');
 const generateText = document.getElementById('carta-gerada');
 
 function createText() {
-  const text = inputLetter.value;
-  const textSplit = text.split(' ');
-  console.log(textSplit);
   generateText.innerText = '';
+  const text = inputLetter.value;
+  console.log(text.length);
+  const textSplit = text.split(' ');
+  if (text.replace(/\s/g, '').length === 0) {
+    generateText.innerText = 'por favor, digite o conteúdo da carta.';
+  }
   for (let i = 0; i < textSplit.length; i += 1) {
     const createSpan = document.createElement('span');
     createSpan.innerText = textSplit[i];
